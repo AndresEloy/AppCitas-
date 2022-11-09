c9f9b82e3f6ba59580dee247e2f3a2d28757498f
AndresEloy <eloy.escobedo@outlook.com>
2022-11-09 07:53:40 -0600

    9/11/22 Segunda Version

---
+++ AppCitas.Service/Controllers/MessageController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class MessageController : BaseApiController
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMessageRepository _messageRepository;
+    private readonly IMapper _mapper;
+
+    public MessageController(IUserRepository userRepository, IMessageRepository messageRepository)
+    {
+        _userRepository = userRepository;
+        _messageRepository = messageRepository;
+        _mapper = _mapper;
+    }
+
+    [HttpPost]
+
+    public async Task<ActionResult<MessageDto>> CreateMessage(MessageCreateDto messageCreateDto)
+    {
+        var username = Username.GetUsername();
+
+        if (username.ToLower().Equals(messageCreateDto.RecipientUsername.ToLowe()))
+            return BadRequest("You cannot send message yo yourself");
+
+        var sender = await _userRepository.GetUserByUsernameAsync(username);
+        var recipient = await _userRepository.GetUserByUsernameAsync(messageCreateDto.Recipient)
+
+        if (recipient == null) return NotFound();
+
+        var message = new Message
+        {
+            Sender = sender,
+            Recipient = recipient,
+            SenderUsername = sender.UserName,
+            RecipientUsername = recipient.UserName,
+            Content = messageCreateDto.Content
+        };
+
+        _messageRepository.AddMessage(message);
+
+        if (await _messageRepository.SaveAllAsync())
+            return Ok(_mapper.Map<MessageDto>(message));
+
+        return BadRequest("Failed to send the message");
+
+
+    }
+}
